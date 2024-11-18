adb5f56cf3c2e3a42e511ddcd1ff03155d071ef0
Santiago Mussi <correagameroficial@gmail.com>
2024-11-18 11:13:17 -0300

    Test

---
+++ src/test/java/com/SzafrankoMussiMasucci/TPO_progra3/TpoProgra3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SzafrankoMussiMasucci.TPO_progra3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpoProgra3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
