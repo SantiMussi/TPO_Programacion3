b790d5fe662e65ee923d300686ca556897997f12
Santiago Mussi <correagameroficial@gmail.com>
2024-11-18 10:59:49 -0300

    first commit

---
+++ src/main/java/com/SzafrankoMussiMasucci/TPO_progra3/TpoProgra3Application.java
@@ -0,0 +1,13 @@
+package com.SzafrankoMussiMasucci.TPO_progra3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpoProgra3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpoProgra3Application.class, args);
+	}
+
+}
