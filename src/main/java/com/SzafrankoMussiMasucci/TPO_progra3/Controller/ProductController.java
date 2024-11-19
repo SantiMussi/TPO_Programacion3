c56a57b5b24312536bbcda8334e1b26453f24ea1
Santiago Mussi <correagameroficial@gmail.com>
2024-11-19 12:30:10 -0300

    Product controller created

---
+++ src/main/java/com/SzafrankoMussiMasucci/TPO_progra3/Controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.SzafrankoMussiMasucci.TPO_progra3.Controller;
+
+import com.SzafrankoMussiMasucci.TPO_progra3.Model.ProductEntity;
+import com.SzafrankoMussiMasucci.TPO_progra3.repository.ProductRepository;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/movies")
+public class ProductController {
+    private final ProductRepository prodRepository;
+    public ProductController(ProductRepository prodRepository) {
+        this.prodRepository = prodRepository;
+    }
+
+    @PutMapping
+    Mono<ProductEntity> createOrUpdateProduct(@RequestBody ProductEntity newProduct) {
+        return prodRepository.save(newProduct);
+    }
+
+    @GetMapping(value = { "", "/" }, produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    Flux<ProductEntity> getProducts() {
+        return prodRepository.findAll();
+    }
+
+
+
+}
