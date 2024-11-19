97c3d50801125a86f81785d7807e869aae3b2d62
Santiago Mussi <correagameroficial@gmail.com>
2024-11-19 12:30:56 -0300

    /Products instead of /movies

--- src/main/java/com/SzafrankoMussiMasucci/TPO_progra3/Controller/ProductController.java
+++ src/main/java/com/SzafrankoMussiMasucci/TPO_progra3/Controller/ProductController.java
@@ -8,7 +8,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/movies")
+@RequestMapping("/products")
 public class ProductController {
     private final ProductRepository prodRepository;
     public ProductController(ProductRepository prodRepository) {
